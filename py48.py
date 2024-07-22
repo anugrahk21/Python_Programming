4ecadd9d5a63063ad29e5e1410d0c5f606e3a51f
ANUGRAH K <anugrah.k910@gmail.com>
2024-07-23 01:23:52 +0530

    mapping and eval,exec function

--- py48.py
+++ py48.py
@@ -3,6 +3,12 @@ x, k = list(map(int, input().split()))# Read input, split by whitespace, convert
 print(x)
 print(k)
 P = input()
-print(eval(P) == k)# here in eval.. it evaluates the function by take the value of x in it.
+print(eval(P) == k)# here in eval (takes string as expression).. it evaluates the function by take the value of x in it.
 a,b=[1,2]
 print(a,b)#a=1,b=2
+
+a=input()
+j=exec(a) #or eval(a)
+if j is not None: #as we can give a print statement for eval oe exec it should only print if the value is not none....otherwise after the result a "none" is printed
+    print(j)
+
