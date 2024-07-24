914b50b17daac493851457298367cecb348da27c
ANUGRAH K <anugrah.k910@gmail.com>
2024-07-24 20:49:39 +0530

    * fun

--- py48.py
+++ py48.py
@@ -1,5 +1,6 @@
 #mapping and eval function
-x, k = list(map(int, input().split()))# Read input, split by whitespace, convert each to int, and create a list...here  x=1(as per sample) k=4
+'''
+x, k = list(map(int, input().split()))# Read input, split by whitespace, convert each to int, and create a list...here  x=1 k=4 if input=1 4
 print(x)
 print(k)
 P = input()
@@ -11,4 +12,14 @@ a=input()
 j=exec(a) #or eval(a)
 if j is not None: #as we can give a print statement for eval oe exec it should only print if the value is not none....otherwise after the result a "none" is printed
     print(j)
+'''
+q,w=map(int,[1,2]) #q,w=map(int,[1,2,3])->error..not enough variables
+print(q)#1
+print(w)#2
 
+e,r=[1,2]#e,r=[1,2,3]->error..not enough variables
+print(e,r) #1 2
+
+e1,*r1=[1,2,3]#e,*r=[1,2,3]-> not an error..here all the other values other than e1(1) will go to r1 as a list
+print(e1,r1) #1 [2,3]
+print(r1[0]) #first index value of r1.....2.
