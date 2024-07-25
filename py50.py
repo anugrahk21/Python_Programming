3875aef63775ccbf91a99af88b8c2cab8964c2ec
ANUGRAH K <anugrah.k910@gmail.com>
2024-07-25 23:46:26 +0530

    Update py50.py

--- py50.py
+++ py50.py
@@ -1,5 +1,7 @@
 a="anugrah"
 b=set(a)
-print(b)
-b.add("xp")
-print(b)
+print(b) #every time to print a set...index of values change
+b.add("xip")
+print(b) #{'r', 'h', 'n', 'a', 'u', 'xip', 'g'}...not every time as it changes every time
+
+#it even includes space as an element.
