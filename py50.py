e0e405f20f78d4eed3801db56f16a00ab5fcecc1
ANUGRAH K <anugrah.k910@gmail.com>
2024-07-26 00:25:18 +0530

    set question added

--- py50.py
+++ py50.py
@@ -5,3 +5,32 @@ b.add("xip")
 print(b) #{'r', 'h', 'n', 'a', 'u', 'xip', 'g'}...not every time as it changes every time
 
 #it even includes space as an element. b.add(" ")
+
+# Input Format
+
+# The first line contains an integer , the total number of country stamps.
+# The next  lines contains the name of the country where the stamp is from.
+# Constraints
+
+
+# Output Format
+
+# Output the total number of distinct country stamps on a single line.
+
+a=int(input(""))
+j=set()
+for i in range(a):
+    h=input()
+    j.add(h)
+k=len(j)
+print(k)
+
+#or
+
+a=int(input(""))
+j=set()
+for i in range(a):
+    h=input()
+    j.add(h)
+k=len(j)
+print(k)
